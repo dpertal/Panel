651bb99136ca1cb27cc866c9dfe51792cb41223c
Moncho Varela <nakome@gmail.com>
2014-10-30 14:23:14 +0100

    Add new language

---
+++ library/language/gr.php
@@ -0,0 +1,82 @@
+<?php 
+    
+    $lang = array(
+        
+        
+        // login
+        'Login' => 'Σύνδεση',
+        'Hello Admin' => 'Γεια σου διαχειριστή',
+        'You need provide a password' => 'Χρειάζεται να δώσετε έναν κωδικό πρόσβασης',
+        
+
+
+        // menu
+        'Home' => 'Αρχική',
+        'Images' => 'Εικόνες',
+        'New Image' => 'Νέα εικόναe',
+        'New Page' => 'Νέα σελίδα',
+        'Uploads' => 'Mεταφορτώσεις' ,
+        'Settings' => 'Ρυθμίσεις',
+        'Documentation' => 'Τεκμηρίωση',
+
+
+
+        // pages
+        'Pages' => 'Σελίδες',
+        'Blog' => 'Blog',
+        'Edit' => 'Επεξεργασία',
+        'Delete' => 'Διαγραφή',
+
+
+
+        // footer
+        'Powered by' => 'Powered by',
+
+
+
+        // New page & update
+        'Name' => 'Όνομα',
+        'Check this if is a blog' => 'Επιλέξτε αυτό αν είναι ένα blog',
+        'Title' => 'Τίτλος',
+        'New Description' => 'Νέα Περιγραφή',
+        'Keywords here' => 'Λέξεις-κλειδιά εδώ' ,
+        'Author' => 'Συγγραφέας',
+        'January 11, 2014' => 'January 11, 2014',
+        'Tags here' =>'Ετικέτες εδώ',
+        'index' => 'index',
+        'Cancel' => 'Ακύρωση',
+        'Save' => 'Αποθήκευση',
+        'Update' => 'Ενημέρωση',
+
+
+
+        // notification
+        'The File' => 'Το αρχείο',
+        'has been deleted' =>'έχει διαγραφεί',
+        'has been save' => 'έχει αποθηκευτή',
+
+
+        // notification uploads
+        'An error ocurred when uploading.' => 'Προέκυψε ένα σφάλμα κατά τη μεταφόρτωση.',
+        'Please ensure you are uploading an image.' => 'Παρακαλούμε βεβαιωθείτε ότι έχετε μεταφόρτωση μιας εικόνας.',
+        'Unsupported filetype uploaded.' => 'Μη υποστηριζόμενος τύπος μεταφόρτωσης αρχείου.',
+        'File uploaded exceeds maximum upload size.' => 'Αρχείο που μεταφορτώθηκε υπερβαίνει το μέγιστο μέγεθος για επιφόρτωση.',
+        'File with that name already exists.' => 'Το αρχείο υπάρχει ήδη αυτό το όνομα.',
+        'Error uploading file - check destination is writeable.' => 'Σφάλμα μεταφόρτωση του αρχείου - ελέγξτε προορισμός είναι εγγράψιμο.',
+
+
+        // images section
+        'Full images' => 'Πλήρες εικόνες',
+        'Thumbnails' => 'Μικρογραφίες',
+
+
+        // uploads
+        'File uploaded successfully' => 'Το αρχείο μεταφορτώθηκε με επιτυχία',
+        'Full Image' => 'Πλήρες Εικόνα',
+        'Thumbnail Image' => 'Μικρογραφία Εικόνας',
+        'Links of Images' => 'Σύνδεσμοι Εικόνες',
+
+
+        // javascript confirm 
+        'Are you sure' => 'Είστε σίγουρος'
+    );

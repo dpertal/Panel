7531215ead0d67e6fa225c6c66a8271e5fc32f84
dpertal <dpertal@gmail.com>
2014-11-07 19:19:46 +0200

    Update login.php

--- library/login.php
+++ library/login.php
@@ -1,13 +1,13 @@
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="col-md-4 col-md-offset-4">	
-				<form  method="POST" action="?action=login"  class="form-inline">
-					<input type="hidden" name="token" value="<?php echo  Morfy::factory()->generateToken(); ?>">
-					<div class="form-group">
-						<input type="password" name="password" id="password" class="form-control">
-					</div>
-					<input type="submit" class="btn btn-primary" value="<?php echo $lang['Login']; ?>">
-				</form>
+			<form  method="POST" action="?action=login"  class="form-inline">
+				<input type="hidden" name="token" value="<?php echo  Morfy::factory()->generateToken(); ?>">
+				<div class="form-group">
+					<input type="password" name="password" id="password" class="form-control">
+				</div>
+				<input type="submit" class="btn btn-primary" value="<?php echo $lang['Login']; ?>">
+			</form>
 		</div>
 	</div>
 </div>

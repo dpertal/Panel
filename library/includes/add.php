08ec9739f598747d5734c6c4a18c21fac9b7c671
dpertal <dpertal@gmail.com>
2014-11-07 19:22:17 +0200

    Update add.php

--- library/includes/add.php
+++ library/includes/add.php
@@ -1,15 +1,32 @@
 <form class="form" method="POST" action="<?php echo Panel::Root('panel');?>?saveFile=<?php echo $filename; ?>">
 	<input type="hidden" name="token" value="' . Morfy::factory()->generateToken() . '">
 
-	<div class="form-group">
-		<label><?php echo $lang['Name'];?>:</label>	
-		<input type="text" name="filename" class="form-control" value="<?php echo $filename; ?>">
-	</div>
-
-	<div class="form-group">
-		<label class="checkbox-inline">
-	      <input name="isBlog" type="checkbox"> <?php echo $lang['Check this if is a blog'];?>
-	  </label>
+	<div class="row p0">
+		<div class="col-md-6">
+			<div class="form-group">
+				<label><?php echo $lang['Name'];?>:</label>	
+				<input type="text" name="filename" class="form-control" value="<?php echo $filename; ?>">
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="form-group">
+				<label>Select Folder</label>
+				<select name="foldername" id="folder" class="form-control">
+					<option>Select Folder</option>
+					<option value="isBlog">Blog</option>
+					<option value="isPortfolio">Portfolio</option>
+				</select>
+				<script>
+					window.addEventListener('load',function(){
+						var folder = document.querySelector('#folder');
+						folder.addEventListener('change',function(){
+							this.name = this.value;
+							console.log(this.name);
+						});
+					});
+				</script>
+			</div>
+		</div>
 	</div>
 
 	<div class="form-group">
